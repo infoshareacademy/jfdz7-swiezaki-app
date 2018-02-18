7d30e08321b9767521fdc8378aac6287b695f1c8
KamilaKisiel <kisiel.kamila@gmail.com>
2018-02-18 12:32:45 +0100

    add shared components

---
+++ src/shared/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import  from './'
+
+class Header extends Component {
+ render() {
+   return (
+       <React.Fragment></React.Fragment>
+     }
+     }
+}
+
+export default Header
