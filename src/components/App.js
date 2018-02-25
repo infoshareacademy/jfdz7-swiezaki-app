d8fdc767553a0738bbfffdc41ce0e97f7a38157e
KamilaKisiel <kisiel.kamila@gmail.com>
2018-02-25 13:17:29 +0100

    handle submit function

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
+import { Route, BrowserRouter} from 'react-router-dom'
 
 import Header from './shared/Header'
-import Search from './shared/Search'
 import Footer from './shared/Footer'
 import Login from './Login'
+import Search from './shared/Search'
+
 
 
 
 class App extends Component {
     render (){
         return (
+            <BrowserRouter>
             <React.Fragment>
                 <Header/>
-                <Login/>
-                <Search/>
+                <Route path="/" exact component={Login}/>
+                <Route path="/search" component={Search}/>
                 <Footer/>
             </React.Fragment>
+            </BrowserRouter>
         )
     }
 }
