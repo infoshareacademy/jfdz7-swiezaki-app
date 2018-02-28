32d5b74f8cf3ca9fabb6d12d4df0ebac2e7a12ce
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-28 09:34:00 +0100

    Shortened const names

--- src/components/Search/PartsList.js
+++ src/components/Search/PartsList.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class PartsList extends Component {
+
     render() {
+
+        const { name, producer, type, date } = this.props;
+
         return (
             <React.Fragment>
                 <ul>
-                    <li>{ this.props.name } <strong>{ this.props.producer }</strong> { this.props.type } { this.props.date }</li>
+                    <li>{ name } <strong>{ producer }</strong> { type } { date }</li>
                 </ul>
             </React.Fragment>
         );
