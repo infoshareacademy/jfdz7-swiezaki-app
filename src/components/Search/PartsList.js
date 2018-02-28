cbaf3cc339aa7dedff47a31a27aaceeac9c81b11
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-28 08:01:08 +0100

    Created PartsList file to dynamically show chosen parts.json elements

---
+++ src/components/Search/PartsList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PartsList extends Component {
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+
+export default PartsList;
