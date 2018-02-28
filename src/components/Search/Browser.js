0ee1510ddf79979d473ab4500656ff38e0152534
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-28 07:55:33 +0100

    Added radio buttons to Browser comp

--- src/components/Search/Browser.js
+++ src/components/Search/Browser.js
@@ -4,6 +4,11 @@ class Browser extends Component {
     render() {
         return (
             <React.Fragment>
+                <input type="radio" name="truck" value="true" checked />osobowe
+                <input type="radio" name="truck" value="false" />ciężarowe
+                <input type="radio" name="new" value="true" checked />nowe
+                <input type="radio" name="new" value="false" />używane
+                <br/>
                 <input type="text" size="40" />
                 <button>Wyszukaj</button>
             </React.Fragment>
