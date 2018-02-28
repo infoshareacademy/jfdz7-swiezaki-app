2ca586810d2381430213f85b78f03c2f24d97d20
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-28 08:15:57 +0100

    Added temp parts list beneath Search input

--- src/components/Search/PartsList.js
+++ src/components/Search/PartsList.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class PartsList extends Component {
     render() {
         return (
             <React.Fragment>
-
+                <ul>
+                    <li>{ this.props.name } <strong>{ this.props.producer }</strong> { this.props.type } { this.props.date }</li>
+                </ul>
             </React.Fragment>
         );
     }
