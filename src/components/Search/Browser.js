287307a30d3169cad9b6322033e8bf0cc4b49912
Radosław Rocławski <r.roclawski@ankara.pl>
2018-03-02 06:55:29 +0100

    Handled radio buttons switch functionality. Switching tested. Console.log shows proper values

--- src/components/Search/Browser.js
+++ src/components/Search/Browser.js
@@ -6,21 +6,51 @@ class Browser extends Component {
 
     state = {
         parts: [
-            { "id": 1, "truck": false, "new": false, "name": "hamulec", "producer": "Opel", "type": "tarczowy", "date": 1998 },
-            { "id": 2, "truck": false, "new": false, "name": "hamulec", "producer": "Mazda", "type": "tarczowy", "date": 1999 },
-            { "id": 3, "truck": false, "new": false, "name": "hamulec", "producer": "Toyota", "type": "tarczowy", "date": 1995 },
-            { "id": 4, "truck": false, "new": false, "name": "hamulec", "producer": "BMW", "type": "tarczowy", "date": 2009 },
-            { "id": 5, "truck": false, "new": false, "name": "hamulec", "producer": "Fiat", "type": "tarczowy", "date": 1999 }
-        ]
+            { "id": 1, "vahicle": "osobowe", "category": "nowe", "name": "hamulec", "producer": "Opel", "type": "tarczowy", "date": 1998 },
+            { "id": 2, "vehicle": "ciężarowe", "category": "używane", "name": "hamulec", "producer": "Mazda", "type": "tarczowy", "date": 1999 },
+            { "id": 3, "vehicle": "osobowe", "category": "nowe", "name": "hamulec", "producer": "Toyota", "type": "tarczowy", "date": 1995 },
+            { "id": 4, "vehicle": "osobowe", "category": "używane", "name": "hamulec", "producer": "BMW", "type": "tarczowy", "date": 2009 },
+            { "id": 5, "vehicle": "ciężarowe", "category": "nowe", "name": "hamulec", "producer": "Fiat", "type": "tarczowy", "date": 1999 }
+        ],
+
+        vehicle: 'osobowe',
+        category: 'nowe'
+    };
+
+
+    handleChange = ({ target: { name, value } }) => {
+        this.setState({
+            [name]: value
+        });
+
+        name === 'vehicle' ?
+            console.log('samochód') : console.log('rodzaj');
+
     };
 
     render() {
         return (
             <React.Fragment>
-                <input type="radio" name="truck" value="true" checked />osobowe
-                <input type="radio" name="truck" value="false" />ciężarowe
-                <input type="radio" name="new" value="true" checked />nowe
-                <input type="radio" name="new" value="false" />używane
+
+                <input type="radio" name="vehicle" value="osobowe"
+                       onChange={ this.handleChange }
+                       checked={ this.state.vehicle === 'osobowe' }
+                />osobowe
+
+                <input type="radio" name="vehicle" value="ciężarowe"
+                       onChange={ this.handleChange }
+                       checked={ this.state.vehicle === 'ciężarowe' }
+                />ciężarowe
+
+                <input type="radio" name="category" value="nowe"
+                       onChange={ this.handleChange }
+                       checked={ this.state.category === 'nowe' }
+                />nowe
+
+                <input type="radio" name="category" value="używane"
+                       onChange={ this.handleChange }
+                       checked={ this.state.category === 'używane' }
+                />używane
                 <br/>
                 <input type="text" size="40" />
                 <button>Wyszukaj</button>
