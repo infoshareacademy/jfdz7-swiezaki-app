8ee8d34f0c254ae8cdcff241507105a020095c04
Radosław Rocławski <r.roclawski@ankara.pl>
2018-03-06 07:53:55 +0100

    Created new data json file: carParts in data dir. Filled the file with temp data from current Browser state

--- src/components/Search/Browser.js
+++ src/components/Search/Browser.js
@@ -5,18 +5,7 @@ import PartsList from './PartsList';
 class Browser extends Component {
 
     state = {
-        parts: [
-            { "id": 1, "vehicle": "osobowe", "category": "nowe", "name": "hamulec", "producer": "Opel", "type": "tarczowy", "date": 1998 },
-            { "id": 2, "vehicle": "ciężarowe", "category": "używane", "name": "hamulec", "producer": "Mazda", "type": "tarczowy", "date": 1999 },
-            { "id": 3, "vehicle": "osobowe", "category": "nowe", "name": "kierownica", "producer": "Toyota", "type": "tarczowy", "date": 1995 },
-            { "id": 4, "vehicle": "osobowe", "category": "używane", "name": "hAmulec", "producer": "BMW", "type": "tarczowy", "date": 2009 },
-            { "id": 5, "vehicle": "ciężarowe", "category": "nowe", "name": "drzwi", "producer": "Fiat", "type": "tarczowy", "date": 1999 },
-            { "id": 6, "vehicle": "ciężarowe", "category": "nowe", "name": "hamUlec", "producer": "Fiat", "type": "tarczowy", "date": 1999 },
-            { "id": 7, "vehicle": "osobowe", "category": "nowe", "name": "Hamulec", "producer": "Fiat", "type": "tarczowy", "date": 1999 },
-            { "id": 8, "vehicle": "ciężarowe", "category": "nowe", "name": "MASKA", "producer": "Fiat", "type": "tarczowy", "date": 1999 },
-            { "id": 9, "vehicle": "osobowe", "category": "nowe", "name": "hamulec", "producer": "Fiat", "type": "tarczowy", "date": 1999 }
-        ],
-
+        parts: [],
         vehicle: 'osobowe',
         category: 'nowe',
         producer: '',
