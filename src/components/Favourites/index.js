2a44d1aa4b482447cf84e356a249611388c7c3cd
JarekSopinski <jarek.sopinski@gmail.com>
2018-03-07 15:59:59 +0100

    Created template for Favourites component

---
+++ src/components/Favourites/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Favourites extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Obserowane produkty:</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Favourites
