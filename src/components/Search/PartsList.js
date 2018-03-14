02858c44d6a691a134ad2aa565e94dc9c9703f1d
JarekSopinski <jarek.sopinski@gmail.com>
2018-03-14 14:54:24 +0100

    Added btn in Browser/PartsList which sends current part to an array of favourite parts in local storage

--- src/components/Search/PartsList.js
+++ src/components/Search/PartsList.js
@@ -7,6 +7,23 @@ class PartsList extends Component {
         this.props.changeMessageState();
     }
 
+    handleAddToFavsBtn = () => {
+        const currentPart = this.props;
+        let currentFavParts;
+
+        // checking if a list of fav parts has been previously created in LS
+        if ( !localStorage.getItem("carPartsBrowserFavParts") ) {
+            localStorage.setItem("carPartsBrowserFavParts", JSON.stringify([]));
+            currentFavParts = JSON.parse(localStorage.getItem("carPartsBrowserFavParts"));
+        } else {
+            currentFavParts = JSON.parse(localStorage.getItem("carPartsBrowserFavParts"));
+        }
+
+        // adding current part and sending updated array to LS
+        const updatedFavParts = currentFavParts.concat(currentPart);
+        localStorage.setItem("carPartsBrowserFavParts", JSON.stringify(updatedFavParts))
+    }
+
     render() {
 
         const { id, name, producer, type, date } = this.props;
@@ -18,6 +35,8 @@ class PartsList extends Component {
                     <li>
                         <Link to={`/search/${ id }`}>{ name } | <strong>{ producer } </strong>
                             | { type } | { date }</Link>
+                        <br/>
+                        <button onClick={ this.handleAddToFavsBtn }>Dodaj do ulubionych</button>
                     </li>
                 </ul>
             </React.Fragment>
