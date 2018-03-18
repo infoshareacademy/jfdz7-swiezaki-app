28bf0b54e0e640603cc7ddb5eca9d7e4a9220a55
KamilaKisiel <kisiel.kamila@gmail.com>
2018-03-18 12:04:17 +0100

    config firebase

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import FacebookAuth from './FacebookAuth'
 
 
 class Login extends Component {
-    state = {
-        username: null
-    };
-
-    onFacebookLogin = (loginStatus, resultObject) => {
-        loginStatus
-            ?
-            this.setState({username:resultObject.user.name})
-            :
-            alert('Błąd logowania')
-    }
 
        handleSubmit = () => {
         this.props.history.push('/search');
     };
 
     render() {
-        const { username } = this.state;
         return (
             <React.Fragment>
                 <label>Adres e-mail</label><br/><input type="email"/><br/>
@@ -29,24 +16,6 @@ class Login extends Component {
                 <button onClick={this.handleSubmit}>Zaloguj</button>
                 <input type="checkbox"/><label>Zapamiętaj mnie</label>
                 <p>Nie masz jeszcze konta? <Link to="/register/">Zarejestruj się!</Link></p>
-                <div>
-                    { !username &&
-                    <div>
-                        <FacebookAuth onLogin={this.onFacebookLogin}>
-                            <div className="fb-login-button"
-                                 data-max-rows="1"
-                                 data-size="medium"
-                                 data-button-type="login_with"
-                                 data-show-faces="false"
-                                 data-auto-logout-link="false"
-                                 data-use-continue-as="false"> </div>
-                        </FacebookAuth>
-                    </div>
-                    }
-                    {username &&
-                    <p>Witaj {username}</p>
-                    }
-                </div>
             </React.Fragment>
         )
     }
---
+++ src/setupFirebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyClQH7vrjvx5bEkPUjRwryTFGFVzUMWYg0",
+    authDomain: "myawsomeproject-82083.firebaseapp.com",
+    databaseURL: "https://myawsomeproject-82083.firebaseio.com",
+    projectId: "myawsomeproject-82083",
+    storageBucket: "myawsomeproject-82083.appspot.com",
+    messagingSenderId: "90810043861"
+};
+firebase.initializeApp(config);
