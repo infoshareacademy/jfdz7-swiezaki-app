4d2aeef15c17e5495d6aed7823073ef7375dc6ea
KamilaKisiel <kisiel.kamila@gmail.com>
2018-03-23 21:56:55 +0100

    Auth connected with main index.js file

--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
+import Auth from '../src/components/Authorisation/Auth'
 import App from './components/App';
 import store from './store';
 
 
 ReactDOM.render(
     <Provider store={ store }>
+        <Auth>
         <Router>
             <App />
         </Router>
+        </Auth>
     </Provider>,
     document.getElementById('root')
 
