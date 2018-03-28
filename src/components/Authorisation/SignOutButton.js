83c7086d26a6942ec6e0bd693b5c7563e0bbd369
KamilaKisiel <kisiel.kamila@gmail.com>
2018-03-28 22:35:05 +0200

    add log out icon

--- src/components/Authorisation/SignOutButton.js
+++ src/components/Authorisation/SignOutButton.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { signOut } from '../../state/auth'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Icon } from 'semantic-ui-react';
+
+import { signOut } from '../../state/auth';
 
 class SignOutButton extends Component {
     render() {
-        return <button onClick={() => this.props.signOut()}>Wyloguj...</button>
+        return <Icon name='sign out' onClick={() => this.props.signOut()}>Wyloguj...</Icon>
     }
 }
 
