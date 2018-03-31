dac9825176d45b1bf16b6cb97f315bdc6e2325ac
KamilaKisiel <kisiel.kamila@gmail.com>
2018-03-31 15:08:25 +0200

    grid container vh centered

--- src/components/Authorisation/Auth.js
+++ src/components/Authorisation/Auth.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Tab, Grid, Divider, Button, Icon } from 'semantic-ui-react';
+import { Button, Header, Divider, Grid, Icon, Tab } from 'semantic-ui-react';
 
 import SignUpForm from './SignUpForm'
 import SignInForm from './SignInForm'
+
+import '../../css/authorisation.css'
+
 
 const panes= [
     { menuItem: 'Logowanie', render: () => <Tab.Pane><SignInForm /></Tab.Pane>},
@@ -14,14 +17,23 @@ const panes= [
 class Auth extends Component {
     render() {
         return this.props.user === null ? (
-            <Grid centered columns={4}>
+
+            <Grid container columns={3} centered={4} className='col-centered'>
+                <Header as="h2" textAlign='center'>
+                    <Header.Content>
+                    Wyszukiwarka części samochodowych
+                </Header.Content>
+                </Header>
+                <Grid.Row className='row-centered'>
                 <Grid.Column>
                     <Tab panes={panes} defaultActiveIndex={0} />
                     <Divider horizontal>lub</Divider>
                     <Button fluid color='facebook'>
                         <Icon name='facebook' /> Kontynuuj z Facebook
                     </Button>
+
                 </Grid.Column>
+                </Grid.Row>
             </Grid>
         ) : ( this.props.children )
     }
