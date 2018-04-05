77f6a14977c87223f239b6de6fda7c862f0cfbad
Radosław Rocławski <r.roclawski@ankara.pl>
2018-04-05 08:36:33 +0200

    Created menu in header section

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import { Container, Menu } from 'semantic-ui-react';
 import SignOutButton from '../Authorisation/SignOutButton';
 
 class Header extends Component {
+
+    state = { activeItem: 'Wyszukiwarka' }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
  render() {
+
+     const { activeItem } = this.state;
+
    return (
        <Container>
 
-           <SignOutButton />
-           <menu>
-               <Link to={`/`}>
-                   [ Wyszukiwarka ]
+           <Menu pointing style={{"margin": "10px 0"}}>
+               <Link to={'/'}>
+               <Menu.Item
+                   name='Wyszukiwarka'
+                   active={activeItem === 'Wyszukiwarka'}
+                   onClick={ this.handleItemClick }
+               />
                </Link>
-                &nbsp;
-                <Link to={`/favourites`}>
-                    [ Ulubione ]
-                </Link>
-           </menu>
+
+               <Link to={'/favourites'}>
+               <Menu.Item
+                   name='Ulubione'
+                   active={activeItem === 'Ulubione'}
+                   onClick={ this.handleItemClick }
+               />
+               </Link>
+                   <Menu.Menu position='right'>
+                       <Menu.Item name='Wyloguj' />
+                   </Menu.Menu>
+           </Menu>
+
+           {/*<SignOutButton />*/}
+               {/*/!*<Link to={`/`}>*!/*/}
+                    {/*<Button onClick={ () => { document.location.href = "/"; }}>*/}
+                   {/*Wyszukiwarka*/}
+                    {/*</Button>*/}
+               {/*/!*</Link>*!/*/}
+                {/*<Button>*/}
+                {/*<Link to={`/favourites`}>*/}
+                    {/*Ulubione*/}
+                {/*</Link>*/}
+                {/*</Button>*/}
 
        </Container>
             )
