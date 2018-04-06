e214bff35e3f17798197b5bd17dce3431001f696
KamilaKisiel <kisiel.kamila@gmail.com>
2018-04-06 22:23:36 +0200

    displaying user name extracted from email address

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -21,6 +21,7 @@ class Header extends Component {
  render() {
 
      const { activeItem } = this.state;
+     const email  = this.props.user.email;
 
    return (
        <Container>
@@ -46,7 +47,7 @@ class Header extends Component {
                        <Menu.Item name='Wyloguj' onClick={ this.props.signOut } />
                    </Menu.Menu>
            </Menu>
-           <p style={{"float":"right"}}>Zalogowany:{' ' + this.props.user.email}</p>
+           <p style={{"float":"right"}}>Zalogowany:{' ' + email.split("@")[0]}</p>
        </Container>
             )
      }
